29efddbf4295a859a19bb2336f416452d8aef6ac
sigmavcs <jchamath+sigma@gmail.com>
2019-08-29 17:37:07 +0530

    2-1

--- sigma-001/lambda.js
+++ sigma-001/lambda.js
@@ -1,4 +1,20 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed..."});
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitomyPool,
+        AttributesToGet: [
+            "sub",
+            "address",
+            "gender",
+            "family_name"
+        ],
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            callback(error);
+        }
+        callback(null, { "message": "Successfully executed..." });
+    });
 }
